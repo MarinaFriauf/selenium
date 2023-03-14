914f6d0d72cde2b0576966068fcc15f93810f40c
Marina Friauf <marina506506506@gmail.com>
2023-03-14 21:07:31 +0100

    correct

--- src/test/java/AppOrderPositiveTest.java
+++ src/test/java/AppOrderPositiveTest.java
@@ -17,40 +17,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppOrderPositiveTest {
     private WebDriver driver;
 
-    //    @BeforeAll
-//    static void configureWebdriver() {
-//        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-//    }
     @BeforeAll
-    public static void setup() { WebDriverManager.chromedriver().setup(); }
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-   @BeforeEach
+    @BeforeEach
     public void createBrowser() {
-       ChromeOptions options = null;
-       options = new ChromeOptions();
-       options.addArguments("--disable-dev-shm-usage");
-       options.addArguments("--no-sandbox");
-       options.addArguments("--headless");
-       driver = new ChromeDriver(options);
-       driver.get("http://localhost:9999");
-   }
+        ChromeOptions options = null;
+        options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
 
     @AfterEach
     public void tearDown() {
         driver.quit();
         driver = null;
     }
+
     @Test
-    void shouldTestSuccessOrderIfCorrectFilling() {
+    void shouldTestSuccess() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иван Иванов");
-        elements.get(1).sendKeys("+79231335015");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79231335015");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
     }
-
-
 }
